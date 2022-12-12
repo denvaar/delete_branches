5a75631f3d0bbaa914374f59bb80130ee4d628f2
Denver Smith <denverpsmith@gmail.com>
2022-12-12 13:25:35 -0700

    Create delete_branches.sh

---
+++ delete_branches.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BRANCHES=$(git branch | grep -E -v "(main|\*)" | sed 's/^  //' | tr '\n' ' ')
+
+if [[ -z "$BRANCHES" ]]
+then
+    echo "No branches to delete."
+    exit 0
+fi
+
+echo -e "${BRANCHES// /\n}"
+
+read -p "Delete these branches [Yy/Nn] ? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "$BRANCHES" | xargs git branch -D
+fi
